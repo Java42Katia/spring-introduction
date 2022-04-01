dfb75e60546ff65f60c4e87be21aa7fbb7ec2db5
katiaGurtman <katia.gurtman@gmail.com>
2022-04-01 17:23:45 +0300

    creates

---
+++ src/main/java/telran/spring/Sender.java
@@ -0,0 +1,15 @@
+package telran.spring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sender {
+	
+	public Sender() {
+		System.out.println("Sender constructor");
+	}
+	
+	public String getSenderGreeting() {
+		return "Hi, I'm a sender";
+	}
+}
